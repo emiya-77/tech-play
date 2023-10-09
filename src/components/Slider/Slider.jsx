8b1bf30e24f96c50e2fbb1e34f570b844d6f23c6
Golam Kibria <uchihai850@gmail.com>
2023-10-09 23:59:15 +0600

    final changes

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -5,7 +5,7 @@ const Slider = () => {
         <>
             <div className="carousel w-full">
                 <div id="slide1" className="carousel-item relative z-1 w-full h-screen">
-                    <img src="/slider/1.jpg" className="w-full object-cover" />
+                    <img src="/data/home/1.jpg" className="w-full object-cover" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2" style={{
                         zIndex: 6,
                     }}>
@@ -14,7 +14,7 @@ const Slider = () => {
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full h-screen">
-                    <img src="/slider/2.jpg" className="w-full object-cover" />
+                    <img src="/data/home/2.jpg" className="w-full object-cover" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2" style={{
                         zIndex: 6,
                     }}>
@@ -23,7 +23,7 @@ const Slider = () => {
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full h-screen">
-                    <img src="/login_bg-3.jpg" className="w-full object-cover" />
+                    <img src="/data/home/3.jpg" className="w-full object-cover" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2" style={{
                         zIndex: 6,
                     }}>
@@ -32,7 +32,7 @@ const Slider = () => {
                     </div>
                 </div>
                 <div id="slide4" className="carousel-item relative w-full h-screen">
-                    <img src="/slider/4.jpg" className="w-full object-cover" />
+                    <img src="/data/home/4.jpg" className="w-full object-cover" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2" style={{
                         zIndex: 6,
                     }}>
